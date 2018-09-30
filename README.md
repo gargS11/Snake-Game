4003ace39b3caf7bea43c30b988572582a22450a
gargS11 <sgarg.cse16@chitkarauniversity.edu.in>
2018-10-01 01:34:40 +0530

    feature if levels is added

---
+++ README.md
@@ -0,0 +1,40 @@
+# Snake-Game
+
+A classic Snake Game in C
+
+## OS
+
+Windows
+
+## Instructions
+
+The game has *two* levels:
+1. **LEVEL 1-** 
+* Generates food every time at random position where snake is not present. 
+* Score increases by 2 and length of snake increases by one whenever snake eats food.
+* Snake can pass through the boundaries.
+* If snake collides with itself or u try to move it in the opp. direction of its motion, then snake dies and game gets over.
+* When score = 50, game moves to level 2
+
+2. **LEVEL 2-** 
+* The snake cannot pass through the boundaries.	
+* When score reaches 100, "You Win".
+
+There is also a feature of saving the game(By pressing `s`).So when u start the game
+* `Press 1` to start a new game Or
+* `Press 2` to start your saved game.
+
+*Note-* The snake does not move continuously.
+
+### Controls
+
+* `Press l` - to move left.
+* `Press r` - to move right.
+* `Press u` - to move up.
+* `Press d` - to move down.
+* `Press s` - to save the game and exit.
+* `Press e` - to exit without saving the game.		 	 
+
+## Authors / Credits
+
+* **Sparsh Garg**
--- snakegame.c
+++ snakegame.c
@@ -1,36 +1,32 @@
- //SNAKE GAME FINAL INCLUDING HASHING AND HAVING OPTION OF SAVING A FILE.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-int R = 18;
-int C = 24; 
-struct snake{
+#include <time.h>  //for srand()
+
+int R = 18; //matrix board length
+int C = 24; //matrix board width
+//struct of a snake node
+struct snake{  
   int x, y;
   char value;
   struct snake *next;
 };
+//struct of food
 struct food {
   int x, y;
   char value;
 };
+//struct of a node for the hash
 struct node{
   int x, y;
   struct node *next; 
 };
-struct hashtable{
+//hash structure
+struct hashtable{ 
   int count;
   struct node *head;
 };
-void displayMatrix(char matrix[R][C]){
-  int i, j;
-  for(i = 0; i < R; i++)
-  {
-    for(j = 0; j < C; j++)
-      printf("%c ", matrix[i][j]);
-    printf("\n");
-  }
-}
+//deleting a node in hash
 void deleteHashNode(struct hashtable *htable, int x, int y){
   int key = x + y;
   htable[key].count--;
@@ -49,6 +45,7 @@ void deleteHashNode(struct hashtable *htable, int x, int y){
     prev->next = temp->next;
   }
 }
+//inserting a node in hash
 void insertHashNode(struct hashtable *htable, int x, int y){
     int key = x + y;
     htable[key].count++;
@@ -59,31 +56,7 @@ void insertHashNode(struct hashtable *htable, int x, int y){
     n->next = htable[key].head;
     htable[key].head = n;
 }
-void transferSnakeToHash(struct snake *shead, struct hashtable *htable){
-  struct snake *ptr;
-  ptr = shead->next;
-  int key;
-  while(ptr != NULL){
-    insertHashNode(htable, ptr->x, ptr->y);
-    ptr = ptr->next;
-  }
-}
-int checkCollision(struct hashtable *htable, int x, int y){
-  int key = x + y;
-  if(htable[key].count > 0)
-  {
-    struct node *ptr;
-    ptr = htable[key].head;
-    while(ptr != NULL)
-    {
-      if(ptr->x == x && ptr->y == y){
-	    return 0;
-      }
-      ptr = ptr->next;
-    }
-  }
-  return 1;
-}
+//inserting a node in snake
 struct snake* insertNode(struct snake *shead, int a, int b){
     struct snake *n, *ptr;
     n = (struct snake *)malloc(sizeof(struct snake));
@@ -104,13 +77,15 @@ struct snake* insertNode(struct snake *shead, int a, int b){
     }
     return shead;
 }
+//swapping two integers
 void swap(int *a, int *b){
   int t = *a;
   *a = *b;
   *b = t;
 }
-struct snake* changeSnakeDirection(struct hashtable *htable,struct snake *shead,
-                                   char direction,char matrix[R][C],int flag)
+//moving snake according to the direction and updating in hash
+struct snake* changeSnakeDirection(struct hashtable *htable,
+ struct snake *shead,char direction,char matrix[R][C],struct food sfood)
 {
    int prevx = shead->x, prevy = shead->y;
    insertHashNode(htable, prevx, prevy);
@@ -133,20 +108,22 @@ struct snake* changeSnakeDirection(struct hashtable *htable,struct snake *shead,
                  shead->x = 1;
        break;
     }
-    struct snake *ptr;
-    ptr = shead->next;
-    while(ptr != NULL){
-        swap(&prevx, &ptr->x);
-        swap(&prevy, &ptr->y);
-        ptr = ptr->next;
-    }
-   if(flag == 1){
-    matrix[prevx][prevy] = ' ';
-    deleteHashNode(htable, prevx, prevy); 
+   struct snake *ptr;
+   ptr = shead->next;
+   while(ptr != NULL){
+      swap(&prevx, &ptr->x);
+      swap(&prevy, &ptr->y);
+      ptr = ptr->next;
    }
-  else
-    shead = insertNode(shead, prevx, prevy);
-  return shead;
+   //if snake eats food then new node is added to the tail of snake
+   if(shead->x == sfood.x && shead->y == sfood.y)
+	  shead = insertNode(shead, prevx, prevy);
+   //otherwise tail of snake is deleted from hash as it has move frwrd
+   else{
+    	matrix[prevx][prevy] = ' ';
+        deleteHashNode(htable, prevx, prevy); 
+   }
+   return shead;
 }
 int inserttemp(char *str, char *temp, int i){
 	int l = strlen(temp) - 1;
@@ -166,7 +143,9 @@ void gettemp(int n, char *temp){
 	}
 	temp[j] = '\0';
 }
-void savegame(struct snake *shead, struct food sfood,int score,FILE *fil){
+//saving game to the file
+void savegame(struct snake *shead,struct food sfood,int score,FILE *fil)
+{
   char str[2000];
   char temp[10];
   int i = 0;
@@ -195,11 +174,48 @@ void savegame(struct snake *shead, struct food sfood,int score,FILE *fil){
   fil = fopen("snake.txt", "w");
   fprintf(fil, "%s", str);
   fclose(fil);
-  //printf("%s\n", str);
 }
+//checking if snake collides with board boundary
+int noBoundaryCollision(struct snake *shead){
+  if(shead->x==1 && shead->next->x==R-2 ||
+     shead->x==R-2 && shead->next->x==1 ||
+	 shead->y==1 && shead->next->y==C-2 ||
+	 shead->y>=C-2 && shead->next->y==1)
+    return 0;
+  return 1;
+}
+//checking if snake collides with itself
+int snakeCollision(struct hashtable *htable, int x, int y){
+  int key = x + y;
+  if(htable[key].count > 0)
+  {
+    struct node *ptr;
+    ptr = htable[key].head;
+    while(ptr != NULL)
+    {
+      if(ptr->x == x && ptr->y == y){
+	    return 1;
+      }
+      ptr = ptr->next;
+    }
+  }
+  return 0;
+}
+//displaying matrix board
+void displayMatrix(char matrix[R][C]){
+  int i, j;
+  for(i = 0; i < R; i++)
+  {
+    for(j = 0; j < C; j++)
+      printf("%c ", matrix[i][j]);
+    printf("\n");
+  }
+}
+//inserting food into matrix board
 void transferFoodToMatrix(char matrix[R][C], struct food sfood){
   matrix[sfood.x][sfood.y] = sfood.value;
 }
+//inserting snake into matrix board
 void transferSnakeToMatrix(char matrix[R][C], struct snake *shead){
   struct snake *ptr;
   ptr = shead;
@@ -208,14 +224,6 @@ void transferSnakeToMatrix(char matrix[R][C], struct snake *shead){
     matrix[ptr->x][ptr->y] = ptr->value;
     ptr = ptr->next;
   }
-}
-void initHashTable(struct hashtable *htable){
-	int i;
-	for(i = 0; i < (R+C); i++)
-	{
-		htable[i].count = 0;
-		htable[i].head = NULL;
-	}
 }
 int getnum(char *str, int *i){
   int k = 0;
@@ -227,12 +235,12 @@ int getnum(char *str, int *i){
   *i += 1;
   return k;
 }
+//returning saved snake 
 struct snake* getSavedSnake(struct snake *shead, char *str){
   int i = 0, a, b;
   while(str[i] != 's')
     i++;
-   if(str[i++] == 's')
-   {
+  if(str[i++] == 's'){
     while(str[i] != '\0'){
       a = getnum(str, &i);
       b = getnum(str, &i);
@@ -241,7 +249,8 @@ struct snake* getSavedSnake(struct snake *shead, char *str){
     }
   }
   return shead;
-}
+} 
+//returning saved food
 struct food getSavedFood(struct food sfood, char *str){
   int i = 0, a, b;
   while(str[i] != 'f')
@@ -253,18 +262,49 @@ struct food getSavedFood(struct food sfood, char *str){
   }
   return sfood;
 }
+//returning saved score in the file
 int getSavedScore(char *str){
   int i = 0;
   if(str[i++] == 'S')
     return getnum(str, &i);
 }
+//checking that food should not lie on the snake & boundary
+int isNotValid(struct food sfood, struct hashtable *htable){
+  //checking if food lies on boundary
+  if(sfood.x<=0||sfood.x>=R-1 || sfood.y<=0||sfood.y>=C-1)
+    return 1;
+  int key = sfood.x + sfood.y; //generating key for food coordinates
+  if(htable[key].count > 0){
+    struct node *ptr;
+    ptr = htable[key].head;
+    while(ptr != NULL)
+    {
+      if(ptr->x == sfood.x && ptr->y == sfood.y)
+        return 1;     
+      ptr = ptr->next;
+    }
+  }
+  return 0;     //returning zero if food coords are valid
+}
+//generating random coordinates for food of the snake
 struct food createFood(struct food sfood){
-	srand(time(0));
-	sfood.x = rand() % R-2;
-	sfood.y = rand() % C-2;
-	sfood.value = '*';
+	srand(time(0)); //taking current systim to generate rndom food
+	sfood.x = rand() % R-2;  //x-coordinate
+	sfood.y = rand() % C-2;  //y-coordinate
+	sfood.value = '*';       //setting value of food to *
 	return sfood;
 }
+//making entry of snake in the hash
+void transferSnakeToHash(struct snake *shead, struct hashtable *htable)
+{
+  struct snake *ptr;
+  ptr = shead->next;
+  while(ptr != NULL){
+    insertHashNode(htable, ptr->x, ptr->y);//fn to insert node in hash
+    ptr = ptr->next;
+  }
+}
+//creating snake with 5 nodes
 struct snake* initSnake(struct snake *shead){
   int k = 5, a = 3, b = 4;
   while(k--){
@@ -273,14 +313,25 @@ struct snake* initSnake(struct snake *shead){
   }
   return shead;
 }
-int userInput(){
+//taking usr option to start a new or saved game and returning it
+int userOption(){
   printf(" 1 - To start a New Game\n");
   printf(" 2 - To start the last saved Game\n");
   int option;
   scanf("%d", &option);
   return option;
 }
-void initMatrix(char matrix[R][C]){
+//initializing hashtable every count to zero and head to NULL
+void initHashTable(struct hashtable *htable){
+	int i;
+	for(i = 0; i < (R+C); i++)
+	{
+		htable[i].count = 0;
+		htable[i].head = NULL;
+	}
+}
+//initializing matrix to make a board
+void initMatrix(char matrix[R][C]){ 
   int i, j;
   matrix[0][0] = matrix[0][C-1] = ' ';
   for(i = 1; i < R; i++){
@@ -297,27 +348,36 @@ void initMatrix(char matrix[R][C]){
 }
 int main()
 {
-  int score = 0;
+  int score = 0;  //variable to store score,initially zero
+  int level = 1;  //variable to store level,initially one
   FILE *fil;
-  char matrix[R][C];
-  initMatrix(matrix);
-  struct hashtable *htable=(struct hashtable*)malloc((R+C)*sizeof(struct hashtable));
-  //char initdirection = 'u';
-  struct snake *shead = NULL;
-  struct food sfood;
-  int option = userInput();
+  char matrix[R][C]; 
+  initMatrix(matrix); //initializing matrix
+  
+  //creating an array object of structure hashtable
+  struct hashtable *htable=(struct hashtable*)malloc
+    ((R+C)*sizeof(struct hashtable));
+  initHashTable(htable);  //initializing hashtable
+  struct snake *shead = NULL; //creating head of snake
+  struct food sfood;  //creating object of structure food
+  
+  //asking option from user to start new gane or saved game
+  int option = userOption();
+  system("cls"); //fn to clear the output screen
+  
   if(option == 1)
   {
-    shead = initSnake(shead);
-    transferSnakeToMatrix(matrix, shead);
+    shead = initSnake(shead); //create snake
+    
+    //making snake entry in the hashtable
+    transferSnakeToHash(shead, htable);
     do{
-       sfood = createFood(sfood);
-    }while(matrix[sfood.x][sfood.y] != ' ');
-    transferFoodToMatrix(matrix, sfood);
+       sfood = createFood(sfood); //generating random food
+    }while(isNotValid(sfood, htable));//until it is a valid food
   }
   else
   {
-  	fil = fopen("snake.txt", "r");
+  	fil = fopen("snake.txt", "r"); //opening saved file in read mode
   	if(fil != NULL){
         char str[2000];
         fscanf(fil, "%[^\n]s", str);
@@ -325,65 +385,77 @@ int main()
         score = getSavedScore(str);
         sfood = getSavedFood(sfood, str);
         shead = getSavedSnake(shead, str);
-        transferFoodToMatrix(matrix, sfood);
-    	transferSnakeToMatrix(matrix, shead);
+        transferSnakeToHash(shead, htable);
+        if(score >= 50)  
+          level = 2;
     }
     else{
+    //if(fil==NULL)i.e.unable to open the saved file then print & exit
 		printf("You don't have any saved game\n");
         exit(0);
 	}
   }
-  printf("INITIALIZING\n");
-  initHashTable(htable);
-  transferSnakeToHash(shead, htable);
-  system("cls");
-  printf("\n SCORE:%d\n", score);
-  displayMatrix(matrix);
+  transferFoodToMatrix(matrix, sfood);
+  transferSnakeToMatrix(matrix, shead);
   
   while(1)
   {
-    printf(" PRESS s to save the game\n PRESS e to exit\n ");
+    //for lev1 checking that snake should not collide with itself
+    if(level==1 && !snakeCollision(htable, shead->x, shead->y))
+	{
+      printf("\n SCORE:%d\t\tLEVEL:%d\n", score, level);
+	  displayMatrix(matrix);
+    }
+    //for lev2 snake should not collide neither itself nor boundary
+    else if(level==2 && !snakeCollision(htable, shead->x, shead->y)&&
+	  noBoundaryCollision(shead))
+	{
+      printf("\n SCORE:%d\t\tLEVEL:%d\n", score, level);
+	  displayMatrix(matrix);
+    }
+    //if there is collision then end game
+    else{
+        printf("............\nOops! Collision\n............\n"); 
+		printf("Final Score: %d\n", score);
+        printf("............\nGame Ends\n............");
+		break;
+    }
+  	printf(" PRESS s to save the game\n PRESS e to exit\n ");
     char direction;
     scanf(" %c", &direction);
-    system("cls");
+    system("cls"); //fn to clear the output screen
     if(direction == 's')
     {
-      savegame(shead, sfood, score, fil);
-      printf("..........GAME SAVED................");
+      savegame(shead, sfood, score, fil); //saving game to the file
+      printf("............\nGAME SAVED\n............");
       break;
     }
     else if(direction == 'e'){
-	  printf("............You Exit...............");
+      printf("............\nYour Score: %d\n", score);	
+	  printf("............\nYou Exit\n............");
       exit(0);
     }
     else{
+      //moving snake according to the direction 
+      shead =changeSnakeDirection(htable,shead,direction,matrix,sfood);
+      transferSnakeToMatrix(matrix, shead);
       if(shead->x == sfood.x && shead->y == sfood.y)
       {
-      	 score += 2;
-         shead = changeSnakeDirection(htable,shead, direction, matrix, 0);
-         sfood = createFood(sfood);
-         while(1){
-           if(checkCollision(htable,sfood.x,sfood.y)&&sfood.x >0 && sfood.y >0){
-             break;
-           }
-           else
-             sfood = createFood(sfood);
-         }
+        score += 2; //if food is eaten incrementing score by 2
+        do
+        {
+          sfood = createFood(sfood); //generating random food
+        }while(isNotValid(sfood, htable));//until we get a valid food
         transferFoodToMatrix(matrix, sfood);
       }
-      else{
-        shead = changeSnakeDirection(htable, shead, direction, matrix, 1);
-      }
-      transferSnakeToMatrix(matrix, shead);
-      
-      if(checkCollision(htable, shead->x, shead->y)){
-	     printf("\n SCORE:%d\n", score);
-	     displayMatrix(matrix);
-      }
-      else{
-        printf(".........Game Ends...........");
+    } 
+    //if score passes 50,increment the level to 2
+    if(score==50 && level==1)
+        level++;
+    else if(score == 100) //if score passes 100, game ends
+	{
+        printf("Final Score: 100\n.........\nYou Win\n...........");
         break;
-      }
     }
   }
   return 0;
